3c9aab14009cb0e8c70e3ea4ea03cb1afcb2f589
СюняковаЕ <@223_1>
2017-12-12 21:36:46 +0600

    123

--- Project1/Source2.cpp
+++ Project1/Source2.cpp
@@ -1,31 +1,17 @@
 #include "stdafx.h"
 
-class MyException: public exception
-{
-public:
-	MyException() {};
-	~MyException() {};
+
+int convert() {
+
 
 };
 
-float divide(float a, float b) {
-	if(b==0){
-		throw MyException();
-	}
-	else {
-		return a / b;
-	}
-
-}
 
 int main() {
 
-	try {
-		divide(2, 0);
-	}
-	catch (MyException e){
-		e.what();
-	}
+	
+
+
 
 	system("pause");
 
